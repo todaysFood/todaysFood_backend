7f66dfaa7769a52d05686baaeb88e0f2da39278a
seyeong <seyoung7555@naver.com>
2021-05-01 20:58:59 +0900

    django start

---
+++ my_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'reviews',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
