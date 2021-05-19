9ea7177281bbdcfcbbb78afb295728df8795bf8f
seyeong <seyoung7555@naver.com>
2021-05-19 21:25:30 +0900

    Fix : admin page 접속 오류 해결
    user model에 is_staff 메소드와 is_active 필드 추가하였음

--- accounts/models.py
+++ accounts/models.py
@@ -17,14 +17,14 @@ class UserManager(BaseUserManager):
 
         user = self.model(email=email, password=password, **extra_fields)
         user.set_password(password)
-        user.is_staff = False
+        # user.is_staff = False
         user.is_superuser = False
         user.save(using=self._db)
         return user
 
     def create_superuser(self, email, password, **extra_fields):
 
-        extra_fields.setdefault('is_staff', True)
+        # extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_active', True)
 
@@ -36,20 +36,22 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    id = models.UUIDField(primary_key=True, default=str(uuid.uuid4()))
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     email = models.EmailField(max_length=25, verbose_name="User Email", unique=True)
     password = models.CharField(max_length=128, verbose_name="User Password")
     name = models.CharField(max_length=15, verbose_name="Usr Name")
     nick_name = models.CharField(max_length=15, verbose_name="User Nick Name")
     last_login = models.DateTimeField(auto_now_add=True, verbose_name="Last Login Time")
     is_superuser = models.BooleanField(default=False)
+    # is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
 
     objects = UserManager()
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["password"]
 
-    is_active = True
+    # is_active = True
 
     def __str__(self):
         return self.email
@@ -59,3 +61,6 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def get_nickname(self):
         return self.nick_name
+
+    def is_staff(self):
+        return self.is_superuser
