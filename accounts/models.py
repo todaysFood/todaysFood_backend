782253e1e1f2d058f1d46c99c5f99957a87fefbc
JianKo <bluetoon@naver.com>
2021-05-21 09:56:40 -0400

    createsuperuser 반영

--- accounts/models.py
+++ accounts/models.py
@@ -24,15 +24,17 @@ class UserManager(BaseUserManager):
 
     def create_superuser(self, email, password, **extra_fields):
 
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_active', True)
+        if not email:
+            raise ValueError("The Email must be set")
+        if not password:
+            raise ValueError("The Password must be set")
 
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError("SuperUser must have is_staff = True.")
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError("SuperUser must have is_superuser = True.")
-        return self.create_user(email, password, **extra_fields)
+        user = self.model(email=email, password=password, **extra_fields)
+        user.set_password(password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -61,5 +63,3 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def get_nickname(self):
         return self.nick_name
-
-
--- posts/models.py
+++ posts/models.py
@@ -10,7 +10,7 @@ class Posts(models.Model):
         return self.title
 
     title = models.CharField(max_length=200, verbose_name="제목")
-    pub_date = models.DateTimeField(default=timezone.now(), verbose_name='작성일자')
+    pub_date = models.DateTimeField(default=timezone.now, verbose_name='작성일자')
     author = models.CharField(max_length=15, verbose_name="작성자")
     # author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     content = models.TextField(max_length=300, verbose_name="내용")
