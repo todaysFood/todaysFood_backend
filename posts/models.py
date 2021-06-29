cd6242ffb867111c6d42f8e10cd555fd68f77df3
seyeong <seyoung7555@naver.com>
2021-06-29 21:14:42 +0900

    Modify : 평점 최솟값, 최댓값 설정
    - 0.0~5.0 사이의 실수로 설정

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.utils import timezone
 from django.conf import settings
@@ -13,6 +14,6 @@ class Posts(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='posts', on_delete=models.CASCADE, to_field="id",
                                db_column='author', null=True, verbose_name="작성자")
     content = models.TextField(max_length=300, verbose_name="내용")
-    rating = models.DecimalField(max_digits=2, decimal_places=1, verbose_name="평점")
+    rating = models.DecimalField(max_digits=2, decimal_places=1, validators=[MinValueValidator(0.0),MaxValueValidator(5.0)], verbose_name="평점")
     place_id = models.IntegerField(default=0)
 # Create your models here.
